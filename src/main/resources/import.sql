304fbf6b51900d58ff614c6115e038f037d8a937
Barry van Someren <barry@coffeesprout.com>
2025-03-12 22:47:49 +0100

    Optimize dependencies and configuration for db branch

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,12 @@
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('todo_seq'), 'Introduction to Quarkus', true, 0, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('todo_seq'), 'Hibernate with Panache', false, 1, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('todo_seq'), 'Visit Quarkus web site', false, 2, 'https://quarkus.io');
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('todo_seq'), 'Star Quarkus project', false, 3, 'https://github.com/quarkusio/quarkus/');
+-- Initial data for the Todo application
+-- Used when hibernate.hbm2ddl.auto is set to create-drop 
+-- ID sequence starts at 1 and increments by 50
+
+-- Basic Todo items
+INSERT INTO Todo(id, title, completed, ordering) VALUES (1, 'Introduction to Quarkus', true, 0);
+INSERT INTO Todo(id, title, completed, ordering) VALUES (51, 'Hibernate with Panache', false, 1);
+INSERT INTO Todo(id, title, completed, ordering, url) VALUES (101, 'Visit Quarkus web site', false, 2, 'https://quarkus.io');
+INSERT INTO Todo(id, title, completed, ordering, url) VALUES (151, 'Star Quarkus project', false, 3, 'https://github.com/quarkusio/quarkus/');
+
+-- Set the sequence to start after the highest manually assigned ID
+ALTER SEQUENCE todo_seq RESTART WITH 201;
